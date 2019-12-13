19321d91625d1ddbcd72af62fad2cedd69aad7cf
arghya <arghyaa@virtusa.com>
2019-12-13 19:48:10 +0530

    first commit

---
+++ src/test/java/com/virtusa/qa/api/RunnerTest.java
@@ -0,0 +1,14 @@
+package com.virtusa.qa.api;
+
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.junit4.Karate;
+
+//import cucumber.api.CucumberOptions;
+
+
+@RunWith(Karate.class)
+
+public class RunnerTest {
+    
+}

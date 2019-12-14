48a14a69a6e65f333f5603001033ae4faa918241
arghya <arghyaa@virtusa.com>
2019-12-14 15:40:11 +0530

    added cucumber report reporting

--- src/test/java/com/virtusa/qa/api/RunnerTest.java
+++ src/test/java/com/virtusa/qa/api/RunnerTest.java
@@ -1,14 +1,44 @@
 package com.virtusa.qa.api;
 
 import org.junit.runner.RunWith;
+import org.junit.Test;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.Assert.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+
 
 import com.intuit.karate.junit4.Karate;
 
 //import cucumber.api.CucumberOptions;
 
 
-@RunWith(Karate.class)
-
+//@RunWith(Karate.class)
+//@KarateOptions(tags = {"~@ignore"}) // important: do not use @RunWith(Karate.class) !
 public class RunnerTest {
     
+	 @Test
+	    public void testParallel() {
+	        System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
+	        Results results = Runner.parallel(getClass(), 5);
+	        generateReport(results.getReportDir());
+	        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);        
+	    }
+	    
+	    public static void generateReport(String karateOutputPath) {        
+	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+	        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+	        Configuration config = new Configuration(new File("target"), "demo");
+	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+	        reportBuilder.generateReports();        
+	    }
+	
+	
 }
